de5c371dc7d0df8d6fb70685e39987023bec33c9
Kevin <Kevin@joelcruz>
2024-10-01 01:08:31 -0300

    Exceptions del libro Terminado por ahora - Cruz Kevin

---
+++ tp05_Grupo42/src/ar/edu/unju/escmi/tp5/exceptions/LibroNoDisponibleException.java
@@ -0,0 +1,7 @@
+package ar.edu.unju.escmi.tp5.exceptions;
+
+public class LibroNoDisponibleException extends Exception {
+    public LibroNoDisponibleException(String mensaje) {
+        super(mensaje);
+    }
+}
